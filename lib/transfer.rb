c1069be579f7ad202fd8a751e0637b27f958fc7e
Garth Lilly <garff247@gmail.com>
2019-05-10 05:58:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ attr_accessor :sender, :receiver, :status, :amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @sender.balance > @amount
+    elsif @sender.balance < @amount
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
