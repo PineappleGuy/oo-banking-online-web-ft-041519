81b0449392a4ea0817b3fe683eb1691041a4d093
Garth Lilly <garff247@gmail.com>
2019-05-10 06:00:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,9 @@ attr_accessor :sender, :receiver, :status, :amount
   end
 
   def reverse_transfer
-
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
   end
 
 end
