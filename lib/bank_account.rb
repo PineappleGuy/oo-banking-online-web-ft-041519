c03ab00884bc309b7c668f2d6507b714c3b68c30
Garth Lilly <garff247@gmail.com>
2019-05-10 05:20:15 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,14 @@
 class BankAccount
+
   attr_accessor :balance, :status
   attr_reader :name
+
   def initialize(name, balance = 1000, status = "open")
     @name = name
     @balance = balance
     @status = status
   end
+
+  def deposit(amount)
+    @balance += amount
 end
